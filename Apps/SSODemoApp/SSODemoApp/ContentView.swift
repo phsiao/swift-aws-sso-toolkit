a04644145ee39e84d86d60a45e4b4f052c5d9548
Shawn Hsiao <shawn.hsiao@gmail.com>
2025-03-22 09:58:55 -0400

    chore: update demo layout

--- Apps/SSODemoApp/SSODemoApp/ContentView.swift
+++ Apps/SSODemoApp/SSODemoApp/ContentView.swift
@@ -2,8 +2,9 @@ import SwiftUI
 import AWSSSOToolkit
 @preconcurrency import AWSSTS
 
-struct ContentView: View {
+struct StaticProfileDemoView: View {
   @State var profileViewModel: ProfileViewModel
+  @State var userArn: String = "N/A"
 
   init() {
     let staticProfile = AWSProfile(profileName: "myprofile", profileType: .SSO(session: AWSSSOSession(sessionName: "mysession", startUrl: "https://hsiao.awsapps.com/start", region: "us-east-1"), accountId: "714381189854", roleName: "AdministratorAccess", region: "us-east-1"))
@@ -13,38 +14,51 @@ struct ContentView: View {
 
   var body: some View {
     VStack {
-      VStack {
-        Text("Static AWS Profile")
-          .padding([.top, .bottom], 10)
-        ToolkitProfileView(profile: $profileViewModel)
-          .padding([.bottom], 10)
-        Text("Profile user ARN: \(profileViewModel.userArn)")
-        Button("Get caller identity") {
-          Task {
-            let profileState = profileViewModel.profileState
-            let identityProvider = profileState.identityResolver
-            switch profileState.profile.profileType {
-            case .SSO(let session, _, _, _):
-              let stsConfiguration = try await STSClient.STSClientConfiguration(
-                awsCredentialIdentityResolver: identityProvider,
-                region: session.region
-              )
-              let stsClient = STSClient(config: stsConfiguration)
-              // print("making callerid call")
-              let response = try await stsClient.getCallerIdentity(
-                input: GetCallerIdentityInput())
-              print("done callerid call, response: \(response)")
-            }
+      Text("Static AWS Profile Demo")
+        .padding([.top, .bottom], 10)
+      ToolkitProfileView(profile: $profileViewModel)
+        .padding([.bottom], 10)
+      Text("Profile user ARN: \(userArn)")
+      Button("Get caller identity") {
+        Task {
+          let profileState = profileViewModel.profileState
+          let identityProvider = profileState.identityResolver
+          switch profileState.profile.profileType {
+          case .SSO(let session, _, _, _):
+            let stsConfiguration = try await STSClient.STSClientConfiguration(
+              awsCredentialIdentityResolver: identityProvider,
+              region: session.region
+            )
+            let stsClient = STSClient(config: stsConfiguration)
+            // print("making callerid call")
+            let response = try await stsClient.getCallerIdentity(
+              input: GetCallerIdentityInput())
+            userArn = response.arn!
           }
         }
       }
-      .padding([.bottom], 20)
-      VStack {
-        Text("Managed AWS Profiles")
-        AWSSSOProfilesView()
-      }
-      .padding()
     }
+  }
+}
+
+struct ProfileManagementDemoView: View {
+  var body: some View {
+    VStack {
+      Text("Managed AWS Profiles Demo")
+      AWSSSOProfilesView()
+    }
+  }
+}
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      StaticProfileDemoView()
+      Divider()
+        .padding([.top, .bottom], 20)
+      ProfileManagementDemoView()
+    }
+    .padding()
   }
 }
 
